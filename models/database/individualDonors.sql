d793259911be0c8a44339c6acc6b9b4e253d464c
bobbiro2e <bobbiro2e@gmail.com>
2018-09-21 10:22:49 -0500

    donor table schema and seeds

---
+++ models/database/individualDonors.sql
@@ -0,0 +1,84 @@
+create table cycle2018
+(
+    contributor_id int
+    auto_increment not null,
+contributor_name varchar
+    (255) not null,
+contributor_company varchar
+    (255) not null,
+contributor_city varchar
+    (255) not null,
+total_contribution int not null,
+percent_to_dems int not null,
+percent_to_reps int not null,
+primary key
+    (contributor_id)
+);
+    create table cycle2016
+    (
+        contributor_id int
+        auto_increment not null,
+contributor_name varchar
+        (255) not null,
+contributor_company varchar
+        (255) not null,
+contributor_city varchar
+        (255) not null,
+total_contribution int not null,
+percent_to_dems int not null,
+percent_to_reps int not null,
+primary key
+        (contributor_id)
+);
+        create table cycle2014
+        (
+            contributor_id int
+            auto_increment not null,
+contributor_name varchar
+            (255) not null,
+contributor_company varchar
+            (255) not null,
+contributor_city varchar
+            (255) not null,
+total_contribution int not null,
+percent_to_dems int not null,
+percent_to_reps int not null,
+primary key
+            (contributor_id)
+);
+            create table cycle2012
+            (
+                contributor_id int
+                auto_increment not null,
+contributor_name varchar
+                (255) not null,
+contributor_company varchar
+                (255) not null,
+contributor_city varchar
+                (255) not null,
+total_contribution int not null,
+percent_to_dems int not null,
+percent_to_reps int not null,
+primary key
+                (contributor_id)
+);
+                create table cycle2010
+                (
+                    contributor_id int
+                    auto_increment not null,
+contributor_name varchar
+                    (255) not null,
+contributor_company varchar
+                    (255) not null,
+contributor_city varchar
+                    (255) not null,
+total_contribution int not null,
+percent_to_dems int not null,
+percent_to_reps int not null,
+primary key
+                    (contributor_id)
+);
+
+                    
+
+
