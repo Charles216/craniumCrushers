4d943c765ed03363f182531ef5c2b36c9e2f4626
Elephant_Man <grogan.charlie@gmail.com>
2018-09-22 11:34:57 -0500

    updated seed data

--- database/donorSeeds.sql
+++ database/donorSeeds.sql
@@ -1,4 +1,4 @@
---SEEDS FOR CYCLE 2018
+-- SEEDS FOR CYCLE 2018
 insert into cycle2018s
     (contributor_name, contributor_company, contributor_city, total_contribution, percent_to_dems, percent_to_reps)
 values
@@ -41,7 +41,7 @@ insert into cycle2018s
 values
     ("Simons, James H.", "Retired", "New York, NY", 1049210, 0, 100);
 
---SEEDS FOR CYCLE 2016
+-- SEEDS FOR CYCLE 2016
 insert into cycle2016s
     (contributor_name, contributor_company, contributor_city, total_contribution, percent_to_dems, percent_to_reps)
 values
@@ -83,7 +83,7 @@ insert into cycle2016s
 values
     ("Foster, Paul L.", "Western Refining", "El Paso, TX", 1401800, 0, 100);
 
---SEEDS FOR CYCLE 2014
+-- SEEDS FOR CYCLE 2014
 insert into cycle2014s
     (contributor_name, contributor_company, contributor_city, total_contribution, percent_to_dems, percent_to_reps)
 values
@@ -125,7 +125,7 @@ insert into cycle2014s
 values
     ("Sussman, Donald", "Paloma Partners", "Rye Brook, NY", 408241, 0, 100);
 
---SEEDS FOR CYCLE 2012
+-- SEEDS FOR CYCLE 2012
 insert into cycle2012s
     (contributor_name, contributor_company, contributor_city, total_contribution, percent_to_dems, percent_to_reps)
 values
@@ -167,7 +167,7 @@ insert into cycle2012s
 values
     ("Uihlein, Richard", "Uline Inc", "Lake Forest, IL", 237000, 0, 100);
 
---SEEDS FOR CYCLE 2010
+-- SEEDS FOR CYCLE 2010
 insert into cycle2010s
     (contributor_name, contributor_company, contributor_city, total_contribution, percent_to_dems, percent_to_reps)
 values
@@ -209,414 +209,414 @@ insert into cycle2010s
 values
     ("Deal, Pamela Susan", "NAU Companies", "St Petersburg, MN", 187700, 100, 0);
 
---UPDATES FOR CYCLE 2018
+-- UPDATES FOR CYCLE 2018
 update cycle2018s
 set contributor_name = "Donald S. Sussman"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2018s
 set contributor_name = "Deborah Simon"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2018s
 set contributor_name = "Paul Skjodt & Cynthia Simon-Skjodt"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2018s
 set contributor_name = "Charles R. Schwab"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2018s
 set contributor_name = "Greg Lindberg"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2018s
 set contributor_name = "Robert L. Mercer"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2018s
 set contributor_name = "Charles P. Joyce"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2018s
 set contributor_name = "Fred J. Eychaner"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2018s
 set contributor_name = "Karen A. Wright"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2018s
 set contributor_name = "James H. Simons"
-where contributor_id = 10;
+where id = 10;
 
---UPDATES FOR CYCLE 2016
+-- UPDATES FOR CYCLE 2016
 update cycle2016s
 set contributor_name = "Sheldon G. & Miriam O. Adelson"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2016s
 set contributor_name = "Fred Eychaner"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2016s
 set contributor_name = "James R. & Mary K. Pritzker"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2016s
 set contributor_name = "Donald S. Sussman"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2016s
 set contributor_name = "Paul E. Singer"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2016s
 set contributor_name = "Kenneth C. & Anne Dias Griffin "
-where contributor_id = 6;
+where id = 6;
 
 update cycle2016s
 set contributor_name = "Karen A. Wright"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2016s
 set contributor_name = "Richard Uihlein"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2016s
 set contributor_name = "Charles R. & Helen O. Schwab"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2016s
 set contributor_name = "Paul L. Foster"
-where contributor_id = 10;
+where id = 10;
 
---UPDATES FOR CYCYLE 2014
+-- UPDATES FOR CYCYLE 2014
 update cycle2014s
 set contributor_name = "Paul E. Singer"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2014s
 set contributor_name = "William H. Clark"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2014s
 set contributor_name = "Ronnie Cameron"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2014s
 set contributor_name = "Tatnall Lea Hillman"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2014s
 set contributor_name = "Marsha Z. Laufer"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2014s
 set contributor_name = "Charles R. Schwab"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2014s
 set contributor_name = "John W. Childs"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2014s
 set contributor_name = "Susan L. Groff"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2014s
 set contributor_name = "Paul J. Isaac"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2014s
 set contributor_name = "Donald S. Sussman"
-where contributor_id = 10;
+where id = 10;
 
---UPDATES FOR CYCLE 2012
+-- UPDATES FOR CYCLE 2012
 update cycle2012s
 set contributor_name = "Charles T. Jr. & Charlotte Munger"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2012s
 set contributor_name = "William H. & Elloine Clark"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2012s
 set contributor_name = "Guy M. Bowers"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2012s
 set contributor_name = "Charles R. & Helen O. Schwab"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2012s
 set contributor_name = "Ray Palmer Jr. Oden"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2012s
 set contributor_name = "John A. Jr. & Rita J. Canning"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2012s
 set contributor_name = "Harry P. & Barbara Kamen"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2012s
 set contributor_name = "Jeffery Hurt"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2012s
 set contributor_name = "Marie-Therese Tibbs"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2012s
 set contributor_name = "Richard Uihlein"
-where contributor_id = 10;
+where id = 10;
 
---UPDATES FOR 2010
+-- UPDATES FOR 2010
 update cycle2010s
 set contributor_name = "Bruce N. Berger"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2010s
 set contributor_name = "Ray Palmer Jr. & Dorthy L. Oden"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2010s
 set contributor_name = "Guy M. Bowers"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2010s
 set contributor_name = "Henry D. Lord"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2010s
 set contributor_name = "Harlan & Dian G. Stai"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2010s
 set contributor_name = "Robert J. Perry"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2010s
 set contributor_name = "Robert J. Perry"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2010s
 set contributor_name = "Fred Eychaner"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2010s
 set contributor_name = "David G. Herro"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2010s
 set contributor_name = "Pamela S. Deal"
-where contributor_id = 10;
+where id = 10;
 ----------------------------------------------------------------------------------
---ADD DATA TO DISLPAY NAME COLUMN
---display names for cycle2018s
+-- ADD DATA TO DISLPAY NAME COLUMN
+-- display names for cycle2018s
 ----------------------------------------------------------------------------------
 update cycle2018s
 set display_name = "Sussman"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2018s
 set display_name = "Simon"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2018s
 set display_name = "Skjodt"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2018s
 set display_name = "Schwab"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2018s
 set display_name = "Lindberg"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2018s
 set display_name = "Mercer"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2018s
 set display_name = "Joyce"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2018s
 set display_name = "Eychaner"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2018s
 set display_name = "Wright"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2018s
 set display_name = "Simons"
-where contributor_id = 10;
+where id = 10;
 
---display names for cycle2016s
+-- display names for cycle2016s
 update cycle2016s
 set display_name = "Adelson"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2016s
 set display_name = "Eychaner"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2016s
 set display_name = "Pritzker"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2016s
 set display_name = "Sussman"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2016s
 set display_name = "Singer"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2016s
 set display_name = "Griffin "
-where contributor_id = 6;
+where id = 6;
 
 update cycle2016s
 set display_name = "Wright"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2016s
 set display_name = "Uihlein"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2016s
 set display_name = "Schwab"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2016s
 set display_name = "Foster"
-where contributor_id = 10;
+where id = 10;
 
---display names for cycle 2014
+-- display names for cycle 2014
 update cycle2014s
 set display_name = "Singer"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2014s
 set display_name = "Clark"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2014s
 set display_name = "Cameron"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2014s
 set display_name = "Hillman"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2014s
 set display_name = "Laufer"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2014s
 set display_name = "Schwab"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2014s
 set display_name = "Childs"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2014s
 set display_name = "Groff"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2014s
 set display_name = "Isaac"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2014s
 set display_name = "Sussman"
-where contributor_id = 10;
+where id = 10;
 
---display names for cycle2012s
+-- display names for cycle2012s
 update cycle2012s
 set display_name = "Munger"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2012s
 set display_name = "Clark"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2012s
 set display_name = "Bowers"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2012s
 set display_name = "Schwab"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2012s
 set display_name = "Oden"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2012s
 set display_name = "Canning"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2012s
 set display_name = "Kamen"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2012s
 set display_name = "Hurt"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2012s
 set display_name = "Tibbs"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2012s
 set display_name = "Uihlein"
-where contributor_id = 10;
+where id = 10;
 
---display names for cycle2010s
+-- display names for cycle2010s
 update cycle2010s
 set display_name = "Berger"
-where contributor_id = 1;
+where id = 1;
 
 update cycle2010s
 set display_name = "Oden"
-where contributor_id = 2;
+where id = 2;
 
 update cycle2010s
 set display_name = "Bowers"
-where contributor_id = 3;
+where id = 3;
 
 update cycle2010s
 set display_name = "Lord"
-where contributor_id = 4;
+where id = 4;
 
 update cycle2010s
 set display_name = "Stai"
-where contributor_id = 5;
+where id = 5;
 
 update cycle2010s
 set display_name = "Perry"
-where contributor_id = 6;
+where id = 6;
 
 update cycle2010s
 set display_name = "Perry"
-where contributor_id = 7;
+where id = 7;
 
 update cycle2010s
 set display_name = "Eychaner"
-where contributor_id = 8;
+where id = 8;
 
 update cycle2010s
 set display_name = "Herro"
-where contributor_id = 9;
+where id = 9;
 
 update cycle2010s
 set display_name = "Deal"
-where contributor_id = 10;
+where id = 10;
